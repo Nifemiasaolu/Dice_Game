91fe942bf5f7471fa0a5c2323c771e2d36bca105
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-17 19:06:13 +0100

    Dice roll functionality

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 'use strict';
 
-// Selecting Elements 
+// Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const diceEL = document.querySelector('.dice');
 
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden');
+
+// Functionalizing the Dice Roll
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //   console.log(dice);
+
+  // 2. Display the dice roll
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+
+//   3. Add dice to current Score 
+  if (dice !== 1) {
+  } else {
+  }
+});
